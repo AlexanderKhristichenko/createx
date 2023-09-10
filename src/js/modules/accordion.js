7a94d0f51338e1339d573ef6902e2c2b8445bda2
Olexander Khrystychenko <alexander.khristichenko@gmail.com>
2023-09-10 17:25:01 +0300

    added new page - interior design and added new section - offer

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,37 @@
+export const accordion = () => {
+  const accordionContent = document.querySelectorAll(".accordion__content");
+
+  accordionContent.forEach((item, ind) => {
+    const accordionHeader = item.querySelector(".accordion__header");
+    const accordionDescription = item.querySelector(".accordion__description");
+
+    if (item.classList.contains("accordion__content--open")) {
+      accordionDescription.style.height = `${accordionDescription.scrollHeight}px`;
+    }
+
+    accordionHeader.addEventListener("click", () => {
+      item.classList.toggle("accordion__content--open");
+
+      if (item.classList.contains("accordion__content--open")) {
+        accordionDescription.style.height = `${accordionDescription.scrollHeight}px`;
+      } else {
+        accordionDescription.style.height = "0px";
+      }
+
+      accordionAutoClose(ind);
+    });
+  });
+
+  const accordionAutoClose = (index) => {
+    accordionContent.forEach((item, ind) => {
+      const accordionDescription = item.querySelector(
+        ".accordion__description",
+      );
+
+      if (index !== ind) {
+        item.classList.remove("accordion__content--open");
+        accordionDescription.style.height = "0px";
+      }
+    });
+  };
+};
